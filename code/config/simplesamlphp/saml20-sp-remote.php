4093892cb522796d5036225826fb64f3b87fed01
kosala <kosalamanojeewa@gmail.com>
2020-05-18 14:21:20 +0530

    these are the files want to copy to simplesamlphp module

---
+++ code/config/simplesamlphp/saml20-sp-remote.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!getenv('SIMPLESAMLPHP_SP_ENTITY_ID')) {
+    throw new UnexpectedValueException('SIMPLESAMLPHP_SP_ENTITY_ID is not defined as an environment variable.');
+}
+if (!getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE')) {
+    throw new UnexpectedValueException('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE is not defined as an environment variable.');
+}
+
+$metadata[getenv('SIMPLESAMLPHP_SP_ENTITY_ID')] = array(
+    'AssertionConsumerService' => getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE'),
+    'SingleLogoutService' => getenv('SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE'),
+);
