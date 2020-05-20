d33ebf4b0042fa50097c74b8f8c87917bf8db18b
kosala <kosalamanojeewa@gmail.com>
2020-05-20 15:13:44 +0530

    bug fixes on test run

--- code/config/simplesamlphp/saml20-sp-remote.php
+++ code/config/simplesamlphp/saml20-sp-remote.php
@@ -1,13 +1,13 @@
 <?php
 
-if (!Config::inst()->get('IdpVariables', 'SIMPLESAMLPHP_SP_ENTITY_ID')) {
+if (!getenv('SIMPLESAMLPHP_SP_ENTITY_ID')) {
     throw new UnexpectedValueException('SIMPLESAMLPHP_SP_ENTITY_ID is not defined as an environment variable.');
 }
-if (!Config::inst()->get('IdpVariables', 'SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE')) {
+if (!getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE')) {
     throw new UnexpectedValueException('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE is not defined as an environment variable.');
 }
 
-$metadata[Config::inst()->get('IdpVariables', 'SIMPLESAMLPHP_SP_ENTITY_ID')] = array(
-    'AssertionConsumerService' => Config::inst()->get('IdpVariables', 'SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE'),
-    'SingleLogoutService' => Config::inst()->get('IdpVariables', 'SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE'),
+$metadata[getenv('SIMPLESAMLPHP_SP_ENTITY_ID')] = array(
+    'AssertionConsumerService' => getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE'),
+    'SingleLogoutService' => getenv('SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE'),
 );
