937b7d06908c8122675f173ae3b91f2573b10df8
kosala <kosalamanojeewa@gmail.com>
2020-05-21 13:05:36 +0530

    changing config and meta data with variables removed want to add manually

--- code/config/simplesamlphp/saml20-sp-remote.php
+++ code/config/simplesamlphp/saml20-sp-remote.php
@@ -1,13 +1,15 @@
 <?php
 
-if (!getenv('SIMPLESAMLPHP_SP_ENTITY_ID')) {
-    throw new UnexpectedValueException('SIMPLESAMLPHP_SP_ENTITY_ID is not defined as an environment variable.');
-}
-if (!getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE')) {
-    throw new UnexpectedValueException('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE is not defined as an environment variable.');
-}
+//if (!getenv('SIMPLESAMLPHP_SP_ENTITY_ID')) {
+//    throw new UnexpectedValueException('SIMPLESAMLPHP_SP_ENTITY_ID is not defined as an environment variable.');
+//}
+//if (!getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE')) {
+//    throw new UnexpectedValueException('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE is not defined as an environment variable.');
+//}
+//
+//$metadata[getenv('SIMPLESAMLPHP_SP_ENTITY_ID')] = array(
+//    'AssertionConsumerService' => getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE'),
+//    'SingleLogoutService' => getenv('SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE'),
+//);
 
-$metadata[getenv('SIMPLESAMLPHP_SP_ENTITY_ID')] = array(
-    'AssertionConsumerService' => getenv('SIMPLESAMLPHP_SP_ASSERTION_CONSUMER_SERVICE'),
-    'SingleLogoutService' => getenv('SIMPLESAMLPHP_SP_SINGLE_LOGOUT_SERVICE'),
-);
+//you want to fill metadata here
